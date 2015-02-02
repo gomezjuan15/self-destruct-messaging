84a010c2858203d3f52e7676e3dae64aac23b40a
Juan Gomez <erioly@gmail.com>
2015-02-01 23:21:11 -0500

    downloading pictures and video

--- app/src/main/java/com/treehouse/gaara/selfdestructmessaging/InboxFragment.java
+++ app/src/main/java/com/treehouse/gaara/selfdestructmessaging/InboxFragment.java
@@ -1,12 +1,26 @@
 package com.treehouse.gaara.selfdestructmessaging;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 
 /**
@@ -14,6 +28,7 @@ import android.view.ViewGroup;
  */
 public class InboxFragment extends ListFragment {
 
+    protected List<ParseObject> mMessages;
 
     public InboxFragment() {
         // Required empty public constructor
@@ -28,5 +43,58 @@ public class InboxFragment extends ListFragment {
         return rootView;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
 
+        getActivity().setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery query = new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
+        query.whereEqualTo(ParseConstants.KEY_RECIPIENT_IDS, ParseUser.getCurrentUser().getObjectId());
+        query.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> messages, ParseException e) {
+
+                getActivity().setProgressBarIndeterminateVisibility(false);
+
+                if(e == null) {
+                    mMessages = messages;
+
+                    String[] userNames = new String[mMessages.size()];
+                    int i = 0;
+                    for (ParseObject message : mMessages) {
+                        userNames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
+                        i++;
+                    }
+
+                    MessageAdapter adapter = new MessageAdapter(
+                            getListView().getContext(),
+                            mMessages);
+                    setListAdapter(adapter);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseObject message = mMessages.get(position);
+        String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+        ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+        Uri fileUri = Uri.parse(file.getUrl());
+
+        if(messageType.equals(ParseConstants.TYPE_IMAGE)){
+            Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+            intent.setData(fileUri);
+            startActivity(intent);
+        } else{
+            Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
+            intent.setDataAndType(fileUri, "video/*");
+            startActivity(intent);
+        }
+
+    }
 }
